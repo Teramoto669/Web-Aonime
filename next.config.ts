4c055b4fbc13178d2680eee98fd673208f1c004f
Teramoto669 <rezhashahidzindarb@gmail.com>
2025-10-30 16:09:34 +0700

    Update cache settings in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -70,7 +70,6 @@ const nextConfig: NextConfig = {
     ],
     minimumCacheTTL: 2678400,
     formats: ['image/webp'],
-    quality: 75,
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
